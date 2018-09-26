a9f64c4b3be84546ecb577a9820b6a42a4db27f1
Igor Guerrero <igfgt1@gmail.com>
2018-09-26 08:14:53 -0400

    Put back Breads copyright, sorry I didn't understand this before.

--- BRTest.c
+++ BRTest.c
@@ -1,8 +1,8 @@
 //
 // BRTest.c
 //
-//  Created by Igor Guerrero on 8/7/2018
-//  Copyright (c) 2018 Hodl Wallet INC.
+//  Created by Aaron Voisine on 8/14/15.
+//  Copyright (c) 2015 breadwallet LLC
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
